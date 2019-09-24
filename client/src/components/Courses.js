c1f6c8d202151e7e818e0db35c269c8bd8adc8eb
Justice Trent <justice.trent@verizon.com>
2019-09-24 16:51:21 -0400

    Refactored Courses

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -21,26 +21,21 @@ class App extends React.Component {
     render() {
         console.log("Rendering")
         console.log(this.state)
-        if (this.state.isLoading) {
-            return (<h2>Loading...</h2>)
-        } else {
-            return (
-                <div className="container">
-                    {this.state.courses.map(course => 
-                        <div class="grid-33" key={course.id}>
-                            <a class="course--module course--link" href="course-detail.html">
-                                <h4 class="course--label" >{course.title}</h4>
-                                <h3 class="course--title" >{course.description}</h3>
-                            </a>
-                        </div>
-
-
-                    )
-                    }
-                </div>
-            );
-        }
+        return (this.state.isLoading) ? (<h2>Loading...</h2>) : (
+            <div className="container">
+                {this.state.courses.map(course =>
+                    <div className="grid-33" key={course.id}>
+                        <a className="course--module course--link" href="course-detail.html">
+                            <h4 className="course--label" >{course.title}</h4>
+                            <h3 className="course--title" >{course.description}</h3>
+                        </a>
+                    </div>
+                )
+                }
+            </div>
+        );
     }
+
 }
 
 export default App;
